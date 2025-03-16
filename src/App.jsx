8f2f2f1fd3b765e3f227399565b656d409511aad
Suriya <true.many.032@gmail.com>
2025-03-16 15:06:43 +0700

    main

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './component.jsx'
+
+function App() {
+
+  return (
+    <section>
+      <h1>My First React App on Vite</h1>
+    </section>
+  )
+}
+
+export default App
